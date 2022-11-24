16b834df0d74fe91156d36da2056e129ae80dd4b
DonCosme <109149406+franema@users.noreply.github.com>
2022-11-24 09:26:30 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import TaskAdd from "./Components/TaskAdd";
+import Overview from "./Components/Overview";
+
+function App() {
+  return (
+    <div className="App">
+      <TaskAdd></TaskAdd>
+      
+    </div>
+  );
+}
+
+export default App;
